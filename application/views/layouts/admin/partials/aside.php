78a49c20567317d218f5ba2c30f735da88971380
gurungrajesh1992 <gurungrajesh1992@gmail.com>
2022-04-08 12:12:11 +0545

    aside
    aside

--- application/views/layouts/admin/partials/aside.php
+++ application/views/layouts/admin/partials/aside.php
@@ -344,6 +344,29 @@ $current_user = $this->session->userdata('current_user');
             </li>
           </ul>
         </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link ">
+            <i class="nav-icon fas fa-user"></i>
+            <p>
+              Staff
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="<?php echo base_url('staff/admin/all'); ?>" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>List</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?php echo base_url('staff/admin/form'); ?>" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Add New</p>
+              </a>
+            </li>
+          </ul>
+        </li>
         <li class="nav-header">Settings</li>
         <li class="nav-item">
           <a href="#" class="nav-link ">
