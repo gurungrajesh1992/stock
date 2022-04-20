acd0df4feda7355dafa4b5622552f60648055bd9
chelina maharjan <cheleena.maharjan.3@gmail.com>
2022-04-20 16:22:57 +0545

    view

--- application/modules/opening_master/views/view.php
+++ application/modules/opening_master/views/view.php
@@ -24,7 +24,7 @@
             <div class="col-md-4">
               <div class="form-group">
                 <label>Fiscal Year</label>
-                <input type="text" name="fiscal_year" class="form-control" id="fiscal_year"value="<?php echo set_value('opening_date', (((isset($detail->fiscal_year)) && $detail->fiscal_year != '') ? $detail->fiscal_year : '')); ?>">
+                <?php echo set_value('opening_date', (((isset($detail->fiscal_year)) && $detail->fiscal_year != '') ? $detail->fiscal_year : '')); ?>
               
               </div>
             </div>
@@ -33,7 +33,7 @@
             <div class="col-md-4">
               <div class="form-group">
                 <label>Opening Date</label>
-                <input type="text" name="opening_date" class="form-control" id="opening_date" value="<?php echo set_value('opening_date', (((isset($detail->opening_date)) && $detail->opening_date != '') ? $detail->opening_date : '')); ?>">
+                <?php echo set_value('opening_date', (((isset($detail->opening_date)) && $detail->opening_date != '') ? $detail->opening_date : '')); ?>
                 
               </div>
             </div>
@@ -45,7 +45,7 @@
             <div class="col-md-8">
               <div class="form-group">
                 <label>Remarks</label>
-                <input type="text" name="remark" class="form-control" id="remarks" placeholder="Remarks" value="<?php echo set_value('remarks', (((isset($detail->remarks)) && $detail->remarks != '') ? $detail->remarks : '')); ?>">
+                <?php echo set_value('remarks', (((isset($detail->remarks)) && $detail->remarks != '') ? $detail->remarks : '')); ?>
                 
               </div>
             </div>
@@ -88,27 +88,25 @@
                 ?>
                       <div class="row" style="margin-bottom: 15px;">
                         <div class=" col-md-2">
-                          <input type="text" name="item_name[]" class="form-control" placeholder="Item Name" value="<?php echo $item_detail->item_name; ?>" readonly>
-                          <input type="hidden" name="item_code[]" class="form-control" placeholder="Item Code" value="<?php echo $value->item_code; ?>">
+                          <?php echo $item_detail->item_name; ?>
                         </div>
                         <div class="col-md-2">
-                          <input type="number" name="qty[]" class="form-control" placeholder="Quantity" value="<?php echo $value->qty; ?>" >
+                          <?php echo $value->qty; ?>
                         </div>
                         <div class="col-md-2">
-                          <input type="number" name="unit_price[]" class="form-control" placeholder="Unit Price" value="<?php echo $value->unit_price; ?>" >
+                          <?php echo $value->unit_price; ?>
                         </div>
                         <div class="col-md-3">
                             <?php 
-                            
                             $location=$this->crud_model->get_where_single('location_para', array('id' => $value->location_id));
-                            
+                            echo $location->store_name;
                             ?>
-                            <input type="text" name="location_id[]" class="form-control"  value="<?php echo $location->store_name; ?>" >
+                           
                         
                           
                         </div>
                         <div class="col-md-3">
-                          <textarea name="remarks[]" class="form-control" rows="1" cols="80" autocomplete="off" placeholder="Remarks"><?php echo $value->remarks; ?></textarea>
+                          <?php echo $value->remarks; ?>
                         </div>
                        
                       </div>
